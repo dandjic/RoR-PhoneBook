b7d11edcb715bf0e88360e86e856611dd8db6de5
Djordje Andjic <dandzhich@yandex.com>
2022-11-18 13:16:33 +0100

    ADD crud logic

--- db/migrate/20221118084149_create_friends.rb
+++
@@ -1,6 +0,0 @@
-class CreateFriends < ActiveRecord::Migration[7.0]
-  def change
-    create_table :friends do |t|
-    end
-  end
-end
--- db/migrate/20221118113216_add_to_friend.rb
+++
@@ -1,4 +0,0 @@
-class AddToFriend < ActiveRecord::Migration[7.0]
-  def change
-  end
-end
